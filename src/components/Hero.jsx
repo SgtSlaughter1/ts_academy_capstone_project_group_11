cf1aef19944914cd93b31b71034e3bd072d41854
chuksmili <chukwudimmiliogu0@gmail.com>
2026-03-21 16:29:27 +0100

    Updated Hero file with logo and image, and also added Hero css file along

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,23 +7,25 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="logo">
-        <img src="/components/ui/logo.jpeg" alt="Solar System Logo" />
+        <img src="/src/assets/logo-rem.png" alt="Solar System Logo" />
       </div>
 
+      <div className='flex'>
       <div className="content">
         <h1>
-        Exploring Our <em>Solar System</em> Through <i>Data</i>
-      </h1>
-      <p>Understand the planets not just by name, but by measurable facts. From size and mass to gravity and density, this page breaks down the solar system in a clear, data-driven way.</p>
+          Exploring Our <em>Solar System</em> Through <i>Data</i>
+        </h1>
+        <p>Understand the planets not just by name, but by measurable facts. From size and mass to gravity and density, this page breaks down the solar system in a clear, data-driven way.</p>
 
-      <a href="#factTable"><button className='btn btn-primary'>Explore the Data</button></a>
-      <button className='btn btn-secondary'>Contact Us</button>
-      
+        <a href="#factTable"><button className='btn btn-primary'>Explore the Data</button></a>
+        <button className='btn btn-secondary'>Contact Us</button>
+      </div>
 
       <div className="hero-image">
-        <img src="/components/ui/hero_imagerem.png" alt="an image of a Planet" />
+        <img src="/src/assets/hero_imagerem.png" alt="an image of a Planet" />
       </div>
       </div>
+      
     </section>
   );
 };
